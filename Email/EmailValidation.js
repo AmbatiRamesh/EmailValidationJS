314d0a975856d486fdbecf9c54345c97b4f09761
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-19 14:45:01 +0530

    Added: The email has to have dot symbol after email server name

--- Email/EmailValidation.js
+++ Email/EmailValidation.js
@@ -1,4 +1,4 @@
-const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]{2,}[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-_.][a-zA-Z0-9-]+$");
+const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]{2,}[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z0-9-]+)$");
 function validateEmail(email){
     if(emailRegex.test(email))
         console.log(email + " is a valid EMAIL.");
@@ -6,8 +6,8 @@ function validateEmail(email){
         throw email + " is an invalid EMAIL.";
 }
 try{
-    validateEmail("ramesh@ggg");   //valid
-    validateEmail(".rahul@kkk");  //invalid - email has to start with a letter
+    validateEmail("ramesh@ggg.com");   //valid
+    validateEmail(".rahul@kkkfgd");  //invalid - email has to start with a letter
 }catch(e){
     console.error(e);
 }
