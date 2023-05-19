4d28ef9256d5389d1392737afcb32557247a38be
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-19 14:52:30 +0530

    Added: Validating email with two character county code

--- Email/EmailValidation.js
+++ Email/EmailValidation.js
@@ -1,4 +1,4 @@
-const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]+[A-Za-z0-9]+[- + . _]?[a-zA-Z0-9]+.@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z]+)(\\.[a-zA-Z]+)*$");
+const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]+[A-Za-z0-9]+[- + . _]?[a-zA-Z0-9]+.@[^-_.][a-zA-Z0-9-]+(\\.[a-z]+)(\\.[a-z]{2})*$");
 function validateEmail(email){
     if(emailRegex.test(email))
         console.log(email + " is a valid EMAIL.");
@@ -6,8 +6,8 @@ function validateEmail(email){
         throw email + " is an invalid EMAIL.";
 }
 try{
-    validateEmail("ramesh_am@ggg.co.in");   //valid
-    validateEmail("rahul$kkk@fgd.com");  //invalid - email has to start with a letter
+    validateEmail("ramesh.ambati@ggg.co.in");   //valid
+    validateEmail("rahul_r$kkk@fgd.com.com");  //invalid - email has to start with a letter
 }catch(e){
     console.error(e);
 }
