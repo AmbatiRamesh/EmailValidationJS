1ed9f4b5bb417c9b47d91d8960b10a18b3a0c714
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-05-19 14:49:15 +0530

    Added: Validating email with valid special characters

--- Email/EmailValidation.js
+++ Email/EmailValidation.js
@@ -1,4 +1,4 @@
-const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]{2,}[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z0-9-]+)$");
+const emailRegex = RegExp("^(?=.{3,20}@)[a-zA-Z]+[A-Za-z0-9]+[- + . _]?[a-zA-Z0-9]+.@[^-_.][a-zA-Z0-9-]+(\\.[A-Za-z]+)(\\.[a-zA-Z]+)*$");
 function validateEmail(email){
     if(emailRegex.test(email))
         console.log(email + " is a valid EMAIL.");
@@ -6,8 +6,8 @@ function validateEmail(email){
         throw email + " is an invalid EMAIL.";
 }
 try{
-    validateEmail("ramesh@ggg.com");   //valid
-    validateEmail(".rahul@kkkfgd");  //invalid - email has to start with a letter
+    validateEmail("ramesh_am@ggg.co.in");   //valid
+    validateEmail("rahul$kkk@fgd.com");  //invalid - email has to start with a letter
 }catch(e){
     console.error(e);
 }
